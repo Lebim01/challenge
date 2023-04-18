c3dee321b9c69314f134af683c0ba34fa4206832
Victor Alvarez <victor.alvarez@mtrade.mx>
2023-04-17 18:56:43 -0600

    api url

--- src/const/cicadaEndpoints.ts
+++ src/const/cicadaEndpoints.ts
@@ -1,4 +1,4 @@
-export const ENDPOINT = "67.205.189.142:8000";
+export const ENDPOINT = "fe-challenge.dev-mzt.xyz";
 
 export const CICADA_WS_ENDPOINT = `wss://${ENDPOINT}/websockets/`;
 export const CICADA_PAIRS_ENDPOINT = `https://${ENDPOINT}/pairs`;
